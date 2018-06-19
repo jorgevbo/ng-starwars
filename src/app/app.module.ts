689d6e9ac7ad0c2efc61dd3280cf8acb181fb56f
Pedro J. Molina <pjmolina@gmail.com>
2018-06-19 14:05:14 +0200

    interfaz ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlanetItemComponent } from './planet-item/planet-item.component';
+import { PlanetDetailComponent } from './planet-detail/planet-detail.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetItemComponent,
+    PlanetDetailComponent,
+    PlanetListComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
