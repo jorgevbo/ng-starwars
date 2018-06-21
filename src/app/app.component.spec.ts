0b2f159627d3fff2ccac9a9d99b3be281e81304e
Pedro J. Molina <pjmolina@gmail.com>
2018-06-21 13:05:54 +0200

    fix error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 xdescribe('AppComponent', () => {
-   { beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
